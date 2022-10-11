e67e8f9cffe7a8381d431f42d2a43d45191416fd
EC2 Default User <ec2-user@ip-172-31-18-194.ec2.internal>
2022-10-11 09:48:29 +0000

    Test2.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [data, setData] = useState(null);
+useEffect(() => {
+fetch(`https://q78sk6ga6d.execute-api.us-east-1.amazonaws.com/test`)
+.then((response) => {
+return response.json()
+})
+.then((actualData) => {
+console.log("Writing....");
+setData(actualData.body);
+console.log(actualData.body);
+})
+}, []);
+return (
+<div className="App">
+<header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+Edit <code>src/App.js</code> and save to reload from Github Actions with React2.
+</p>
+<p> {data}
+</p>
+<a
+className="App-link"
+href="https://reactjs.org"
+target="_blank"
+rel="noopener noreferrer"
+>
+Learn React
+</a>
+</header>
+</div>
+);
 }
-
 export default App;
