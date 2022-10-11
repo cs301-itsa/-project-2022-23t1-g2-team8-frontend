3f77c67c19e08d70a734b5691fe6caac8a18f124
EC2 Default User <ec2-user@ip-172-31-18-194.ec2.internal>
2022-10-11 10:26:13 +0000

    remove cors

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import { useEffect, useState } from "react";
 function App() {
 const [data, setData] = useState(null);
 useEffect(() => {
-fetch(`https://wy081tc2nf.execute-api.us-east-1.amazonaws.com/IntegrationTesting`,
-{
-    mode: 'cors',
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-})
+fetch(`https://wy081tc2nf.execute-api.us-east-1.amazonaws.com/IntegrationTesting`)
 .then((response) => {
 return response.json()
 })
